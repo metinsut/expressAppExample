014041440ccf490c0929d4fc3394ec0f06022745
mtnst <metindir@gmail.com>
2018-02-25 16:13:12 +0300

    directors

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const database = require("./database/index");
 const movie = require("./routes/movie");
+const director = require("./routes/director");
 
 const app = express();
 database();
@@ -11,10 +12,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/api/movie", movie);
+app.use("/api/director", director);
 
 const port = process.env.PORT || 3000;
 const server = http.createServer(app);
 
 server.listen(port, () => {
-  console.log("Node API is running...");
+      console.log("Node API is running...");
 });
